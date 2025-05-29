d14a452a6e90df46474073e5ebafeaaf7a9ad11a
zhao-18 <zhao1455@gmail.com>
2025-05-29 00:37:50 -0700

    Add music player on explore page

--- frontend/src/Explore.svelte
+++ frontend/src/Explore.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
-    import type { User } from '../lib/User';
+    import type { User } from './lib/User';
+    import './styles/explore.css';
 
     export let userInfo: User;
+    
+    let trackId: String = "40riOy7x9W7GXjyGp4pjAv";
 </script>
 
 <svelte:head>
@@ -11,11 +14,31 @@
 <main class="main-view">
     <header class="main-header">
         <h1>🔍 Explore Music</h1>
-        <button class="logout-btn" on:click={logout}>Logout</button>
+        <a href="http://localhost:8000/logout">
+            <button class="logout-btn">Logout</button>
+        </a>
+
     </header>
 
-    <section class="content">
+    <section class="content explore-content">
         <p>Discover new music based on your preferences.</p>
         <!-- Add explore content here -->
+        <div id="player">
+            <iframe
+                    title="Player"
+                    style="border-radius:12px"
+                    src={`https://open.spotify.com/embed/track/${trackId}?utm_source=generator&theme=0`}
+                    width="100%"
+                    height="100%"
+                    allowfullscreen={true}
+                    allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+                    loading="lazy"></iframe>
+        </div>
+        <div id="explore-action-buttons">
+            <button>Dislike</button>
+            <button>Save</button>
+            <button>Like</button>
+        </div>
+        <p id="explore-hints">Some text about keyboard shortcut</p>
     </section>
 </main>
