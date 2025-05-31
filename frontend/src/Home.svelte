4931570743ad1c7b14af95c465c95a8a51a7ff39
zhao-18 <zhao1455@gmail.com>
2025-05-31 16:56:26 -0700

    try using image and sample json from documentation

--- frontend/src/Home.svelte
+++ frontend/src/Home.svelte
@@ -1,36 +1,35 @@
 <script lang="ts">
-    import type { User } from '../lib/User';
+    import type { User } from './lib/User';
+
+    // The sample data is from https://developer.spotify.com/documentation/web-api/reference/get-users-saved-tracks
+    // From what I see on spotify documentation, although some fields might differ
+    // the content of array for songs seem to be fairly consistent.
+    // Changing from sample data to normal data should require minimal modification
+    import sampleTracks from './assets/sample_songs.json'
+    import {onMount} from "svelte";
   
     export let userInfo: User;
     export let logout: () => void;
-  
-    const trendingSongs = [
-      {
-        name: "Blinding Lights",
-        artist: "The Weeknd",
-        image: "/images/blinding_lights.png"
-      },
-      {
-        name: "As It Was",
-        artist: "Harry Styles",
-        image: "/images/as_it_was.png"
-      },
-      {
-        name: "Kill Bill",
-        artist: "SZA",
-        image: "/images/kill_bill.png"
-      },
-      {
-        name: "Paint The Town Red",
-        artist: "Doja Cat",
-        image: "/images/paint_red.png"
-      },
-      {
-        name: "good 4 u",
-        artist: "Olivia Rodrigo",
-        image: "/images/good4u.png"
-      }
-    ];
+
+    function transformData(songs: any) {
+      return songs.items.map((song: any) => {
+        return {
+          image: song.track.album.images[0]?.url, // Can put if condition here for default image
+          name: song.track.name,
+          artist: song.track.artists.map((artist: any) => artist.name).join(", "), // More than one artist
+        }
+      });
+    }
+
+    let trendingSongs = [];
+    let recentSongs = [];
+
+    onMount(() => {
+      // Get trending songs and recent songs from backend.
+      // Hopefully transformation can be done on backend side too.
+      trendingSongs = transformData(sampleTracks);
+      recentSongs = transformData(sampleTracks);
+    })
   </script>
   
   <svelte:head>
@@ -52,6 +51,20 @@
               src={song.image}
               alt={song.name}
               style="width: 160px; height: 160px; object-fit: cover; border-radius: 6px;"
+            />
+            <p><strong>{song.name}</strong></p>
+            <p style="font-size: 0.85rem;">{song.artist}</p>
+          </div>
+        {/each}
+      </div>
+      <h2>🎵 Recents</h2>
+      <div class="stack-container">
+        {#each recentSongs as song}
+          <div class="album-card">
+            <img
+                    src={song.image}
+                    alt={song.name}
+                    style="width: 160px; height: 160px; object-fit: cover; border-radius: 6px;"
             />
             <p><strong>{song.name}</strong></p>
             <p style="font-size: 0.85rem;">{song.artist}</p>
